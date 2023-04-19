1b9fd9f4743cfc074379afd3c1ff8ee0db52065f
Abhishek Kattuparambil <abhishekkattuparambil@gmail.com>
2023-04-18 18:36:42 -0700

    project styling

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 import './style.css'
 import p1 from '.././images/p1.jpg'
 import p2 from '.././images/p2.jpg'
-import p3 from '.././images/p3.png'
+import p3 from '.././images/p3.gif'
 import p4 from '.././images/p4.png'
 import p5 from '.././images/p5.png'
 import p6 from '.././images/p6.jpg'
